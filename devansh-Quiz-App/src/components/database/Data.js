ca7a48a4836a71fcdde3b44e4384e27c7940daa5
Devansh12git <167893104+Devansh12git@users.noreply.github.com>
2024-07-29 18:25:14 +0530

    Add files via upload

---
+++ devansh-Quiz-App/src/components/database/Data.js
@@ -0,0 +1,115 @@
+const data =[
+    {
+        "id": 1,
+        "question": "Who was the first President of India?",
+        "options": [
+            "Dr. Rajendra Prasad",
+            "Dr. BR ambedkar",
+            "Mahatam Gandhi ji",
+            "Rahul Gandhi"
+        ],
+
+        "answer": "Dr. Rajendra Prasad"
+    },
+    {
+        "id": 2,
+        "question": "Capital of INDIA",
+        "options": [
+            "Delhi",
+            "Haryana",
+            "Tamil Nadu",
+            "Bihar"
+        ],
+        "answer": "Delhi"
+    },
+    {
+        "id": 3,
+        "question": "Who invented Computer?",
+        "options": [
+            "Charles Babbage",
+            "Charles Smith",
+            "Charles Mathew",
+            "Charles Cambridge"
+        ],
+        "answer": "Charles Babbage"
+    },
+    {
+        "id": 4,
+        "question": "When do we celebrate Independence Day",
+        "options": [
+            "August 15",
+            "August 20",
+            "August 3",
+            "August 31"
+        ],
+        "answer": "August 15"
+    },
+    {
+        "id": 5,
+        "question": "Which device is used to input data into a computer?",
+        "options": [
+            "Monitor",
+            "Keyboard",
+            "Printer",
+            "Hard Drive"
+        ],
+        "answer": "Keyboard"
+    },
+    {
+        "id": 6,
+        "question": "What is the capital of United States",
+        "options": [
+            "Washington, D.C.",
+            "New York",
+            "California",
+            "Brazil"
+        ],
+        "answer": "Washington, D.C"
+    },
+    {
+        "id": 7,
+        "question": "What does GUI stand for?",
+        "options": [
+            "Graphical User Interface",
+            "Graphical Utility Interface",
+            "General User Interface",
+            "Graphical Uniform Interface"
+        ],
+        "answer": "Graphical User Interface"
+    },
+    {
+        "id": 8,
+        "question": "Who wrote Romeo and Juliet?",
+        "options": [
+            "William Shakespeare",
+            "Mahatama Gandhi",
+            "Dr Br Ambedkar",
+            "W.B Yeat"
+        ],
+        "answer": "William Shakespeare"
+    },
+    {
+        "id": 9,
+        "question": "What is the primary function of a computer's BIOS?",
+        "options": [
+            "To manage files and folders",
+            "To provide an interface for user interaction",
+            "To initialize and test hardware during boot-up",
+            "To run application software"
+        ],
+        "answer": "To initialize and test hardware during boot-up"
+    },
+    {
+        "id": 10,
+        "question": "How Many Colours are there in Indian Flag",
+        "options": [
+            "Three",
+            "Two",
+            "Five",
+            "Four"
+        ],
+        "answer": "Three"
+    }
+]
+
+export default data;
